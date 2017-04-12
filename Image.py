205e3400a144bdaf56bd983f83afafe094224c3a
jsh0x <jshox.zap@gmail.com>
2017-04-12 11:34:42 -0600

    Created 'Image' module, started Optical Character Recognition(OCR) function

---
+++ Image.py
@@ -0,0 +1,18 @@
+__author__ = 'jsh0x'
+__version__ = '1.0.0'
+
+import os
+import cv2
+import numpy as np
+from PIL import Image
+
+
+
+def OCR(input_string, haystack_image):
+	if type(input_string) is not str: raise TypeError(f"Value 'input_string' must be type str, instead got type {type(input_string)}")
+	if type(haystack_image) is str and not os.path.exists(haystack_image): raise FileNotFoundError(f"Image directory '{haystack_image}' not found")
+	elif type(haystack_image) is str: img = np.array(Image.open(haystack_image))
+	elif type(haystack_image) is Image.Image: img = np.array(haystack_image)
+	elif type(haystack_image) is np.ndarray: img = haystack_image
+	else: raise TypeError(f"Value 'haystack_image' must be a PIL Image-Object, an image directory, or an image array, instead got {type(haystack_image)}")
+	return
